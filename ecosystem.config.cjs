33829ab9bb092ea66791d371b0f09383f1fcd6f7
Vishnu Kumar <viviku@amazon.com>
2024-12-29 18:35:06 -0800

    fied ecosystem cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'alaska-waitlist',
+      script: '.next/standalone/server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        PORT: 3000,
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}; 
